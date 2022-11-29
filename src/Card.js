7fb8616c600216f04c56d3087ca7f5d8d81a8058
Jake Ketcheson <snowcave@gmail.com>
2022-11-29 04:24:14 +0000

    reprod the failing test by not `mounting` inside of act when floating-ui is using middleware that does stuff in the DOM

--- src/Card.js
+++ src/Card.js
@@ -1,18 +1,16 @@
 import React from "react";
-import ReactDom from "react-dom";
-import { useFloating } from "@floating-ui/react-dom";
+
+import { useFloating, offset, flip, shift } from "@floating-ui/react-dom";
 export const Card = () => {
   const { reference, floating, x, y } = useFloating({
-    placement: "top-end"
+    placement: "top-end",
+    middleware: [offset(10), flip(), shift()],
   });
   return (
     <div className="card">
-      {ReactDom.createPortal(
-        <span ref={floating} style={{ position: "absolute", top: y, left: x }}>
-          Cards
-        </span>,
-        document.body
-      )}
+      <span ref={floating} style={{ position: "absolute", top: y, left: x }}>
+        Cards
+      </span>
       <button ref={reference}>Reference</button>
     </div>
   );
--- src/index.test.js
+++ src/index.test.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Enzyme, { mount } from "enzyme";
-// import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
-import Adapter from "enzyme-adapter-react-16";
+import {act} from 'react-dom/test-utils';
+import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
+// import Adapter from "enzyme-adapter-react-16";
 import { Card } from "./Card";
 Enzyme.configure({ adapter: new Adapter() });
 
-it("another find causing bug", () => {
-  const wrapper = mount(<Card />);
-  console.log(wrapper.html());
-});
+it("another find causing bug", async () => {
+  let wrapper
+  await act(async () => {
+    wrapper = mount(<Card />);
+  });
+  console.log(wrapper.html())
+})
+
