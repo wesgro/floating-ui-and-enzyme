0a1780128b5bf45106d1301c993c341fe614fc09
Jake Ketcheson <snowcave@gmail.com>
2022-12-01 00:29:43 +0000

    add failing example

--- src/index.test.js
+++ src/index.test.js
@@ -1,16 +1,21 @@
 import React from "react";
 import Enzyme, { mount } from "enzyme";
-import {act} from 'react-dom/test-utils';
+import { act } from "react-dom/test-utils";
 import Adapter from "@wojtekmaj/enzyme-adapter-react-17";
 // import Adapter from "enzyme-adapter-react-16";
 import { Card } from "./Card";
 Enzyme.configure({ adapter: new Adapter() });
 
-it("another find causing bug", async () => {
-  let wrapper
+it("this wont fail", async () => {
+  let wrapper;
   await act(async () => {
     wrapper = mount(<Card />);
   });
-  console.log(wrapper.html())
-})
+  console.log(wrapper.html());
+});
 
+it("this will fail", async () => {
+  let wrapper;
+  wrapper = mount(<Card />);
+  console.log(wrapper.html());
+});
